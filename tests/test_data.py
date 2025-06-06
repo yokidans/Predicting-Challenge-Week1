1e8e236e6b241fd5f0cc1d25517d092d7e3264c7
yokidans <teferi6245@gmail.com>
2025-06-06 10:56:56 +0300

    add test update code

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,12 @@
 import pytest
 from src.data.loader import DataLoader
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
+def test_data_loader():
+    loader = DataLoader()
+    assert loader is not None
 class TestDataLoading:
     def test_csv_loading(self):
         loader = DataLoader()
